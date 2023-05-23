0e3ef38c3fb729a279de9bc54f5e7d57db50e695
johnnrodrigues <johnn_rodrigues@hotmail.com>
2023-05-23 15:04:08 -0300

    correcao voltar editativ

--- atividade/editAtv.php
+++ atividade/editAtv.php
@@ -30,7 +30,7 @@ if(isset($_GET["id"]) && !empty($_GET["id"])){
 ?>
 
 <div class="d-grid gap-2 d-md-flex justify-content-md-end" style="margin-top: 1%;">
-    <a href="./tabCategorias.php" class="btn btn-primary">VOLTAR</a>
+    <a href="./tabAtividade.php" class="btn btn-primary">VOLTAR</a>
 </div>
 
 <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
